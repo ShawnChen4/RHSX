45c1326f2a61b7c0c9a7a743183d2365a77978b8
Pineapplehour <thanesapineapple@gmail.com>
2022-05-21 22:34:41 -0400

    Psudocode Version 1

--- Main.java
+++ Main.java
@@ -7,6 +7,28 @@ public class Main {
         ArrayList<Integer> numShares = new ArrayList<Integer>();
         ArrayList<Double> bankRoll = new ArrayList<Double>();
 
+        /*
+        while (game active)
+        asks for buyer's ID
+        asks fer sellers ID
+        asks for number of shares traded
+        asks at what cost basis
+
+        checks if it violates position limits (add and operators)
+        if it violates the limit, reset loop
+        if it does not violate any limit 
+
+        calculates buyer bankroll impact and numshare impact
+        bankroll = bankroll + number of shares traded * cost basis
+        numshare = numshare + number of shares bought
+
+        calculates seller bankroll impact and numbshare impact
+        bankroll = bankroll - number of shares traded * cost basis
+        numshare = numshare - number of shares sold
+
+        prints out the results for everyone 
+        */
+
         
 
     }
