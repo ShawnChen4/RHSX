a5a780160a50b1880cfa802eb1089258220637c9
ShawnChen4 <83143929+ShawnChen4@users.noreply.github.com>
2022-08-26 22:58:55 -0400

    Updated File structure, some OOP changes

--- RHSX.java
+++ RHSX.java
@@ -1,32 +1,11 @@
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
 import java.io.*;
 
-
 public class RHSX {
-    public static void main(String[] args) throws IOException {
-        ArrayList<String> properties = new ArrayList<String>();
-        ArrayList<Trader> traders = new ArrayList<Trader>();
-
-        File database = new File("Database.txt");
-        Scanner readFile = new Scanner(database);
-
-        // Reading Text File Database
-        while (readFile.hasNext()) {
-            properties.add(readFile.next());
-        }
-        
-        for (int i = 0; i < properties.size(); i++) {
-            String[] arr = properties.get(i).split("/", 3);
-            traders.add(new Trader(arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2])));
-        }
-        
-        for (int i = 0; i < traders.size(); i++) {
-            traders.get(i).printTraderInfo();
-        }
-
-        RHSX_Interface GUI = new RHSX_Interface();
-        GUI.run();
-    }
+	
+	public static void main(String[] args) throws IOException {
+	
+		RHSX_Interface GUI = new RHSX_Interface();
+		GUI.collectInfo();
+		GUI.run();
+	}
 }
